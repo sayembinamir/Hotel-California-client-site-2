e0e9208059035ac39ec9d14cd1a21868f37c13dc
sayembinamir <sayembinamir912@gmail.com>
2023-05-03 23:59:19 +0600

    server site create

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const chefrecipes = require('./data/chefs.json');
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Chef Recipe Hunter Server is ready...')
+})
+
+app.get('/chefrecipes', (req, res) => {
+    res.send(chefrecipes);
+})
+
+app.get('/chefrecipes/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedChef = chefrecipes.find(c =>c._id === id);
+    res.send(selectedChef);
+})
+
+app.listen(port, () => {
+    console.log('Server is ready: ', port);
+})
